26a3b10fca10d44755bcc5a244379b927faa591f
lhoeke <l.hoeke@gmx.de>
2024-03-25 18:00:23 +0100

    added models and load models

---
+++ ml_api/app/models/models.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from typing import Dict
+
+class UserRequestIn(BaseModel):
+    """..."""
+
+    text: str = Field(...,
+                     max_length=500,
+                     description="Placeholder"
+                     )
+
+
+class EntityOut(BaseModel):
+
+    text: str = Field(...,
+                     max_length=500,
+                     description="Placeholder"
+                     )
+
+
+class EntitiesOut(BaseModel):
+
+    user_text: Dict[str, str] = Field(...,
+                                         max_length=500,
+                                         description="Placeholder"
+                                        )
---
+++ ml_api/app/tasks/load_models.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+def load_models():
+    """
+    load the models from disk
+    and put them in a dictionary
+    Returns:
+        dict: loaded models
+    """
+
+    print("models loaded from disk")
+    pass
