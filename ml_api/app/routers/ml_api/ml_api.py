6a18249dd4f50f71f2620fe83119c2ed35cef054
lhoeke <l.hoeke@gmx.de>
2024-04-02 19:44:49 +0200

    added ml-api

---
+++ ml_api/app/routers/ml_api/ml_api.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+from typing import Dict
+
+from copy import deepcopy
+
+from fastapi import APIRouter
+import app
+
+from app.models.models import EntitiesOut
+from app.models.models import UserRequestIn
+
+
+router = APIRouter()
+
+@router.post("/ds_api", response_model=EntitiesOut)
+def classify_input(user_request: UserRequestIn):
+    text = user_request.text
+
+    return {"user_text": {"hellooo" : text}}
