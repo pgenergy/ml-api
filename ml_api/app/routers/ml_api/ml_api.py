a29f59a21d9438d51283aa64988dc7e31e65986e
lhoeke <l.hoeke@gmx.de>
2024-05-14 18:32:04 +0200

    added new Request, Response classes

--- ml_api/app/routers/ml_api/ml_api.py
+++ ml_api/app/routers/ml_api/ml_api.py
@@ -6,8 +6,10 @@ from copy import deepcopy
 from fastapi import APIRouter
 import app
 
-from app.models.models import EntitiesOut
-from app.models.models import UserRequestIn
+from app.models.models import UserRequestIn, EntitiesOut
+from app.models.models import DeviceClassificationRequest, DeviceClassificationResponse
+
+
 
 from src.classification.classify_devices import predict
 
@@ -21,10 +23,16 @@ def classify_input(user_request: UserRequestIn):
     return {"user_text": {"Testanfrage" : text}}
 
 
-@router.post("/classify_devices", response_model=EntitiesOut)
-def classify_input(user_request: UserRequestIn):
-    text = user_request.text
+@router.post("/classify_devices", response_model=DeviceClassificationResponse)
+def classify_input(user_request: DeviceClassificationRequest):
+    print(user_request)
 
     test = predict()
 
-    return {"user_text": {"Testanfrage" : text}}
+    test = {
+        "timestamp": "2024-05-10T12:35:00",
+        "power": 1800.0,
+        "classification": "Gefrierschrank"
+    }
+
+    return {"user_text": {"electricity" : test}}
