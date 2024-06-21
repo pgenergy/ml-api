15e5f867b0b373c23e6b19f80b8b618cdee8fc55
Lukas Hein <lukashein@protonmail.com>
2024-06-21 13:07:47 +0200

    async tests

--- ml_api/app/test_main.py
+++ ml_api/app/test_main.py
@@ -1,17 +1,32 @@
+import pytest
+from asgi_lifespan import LifespanManager
+from httpx import AsyncClient, ASGITransport
 from starlette import status
-from fastapi.testclient import TestClient
 
 from .main import app, settings
 
-client = TestClient(app)
+
+@pytest.fixture
+def anyio_backend():
+    # we don't use trio
+    return 'asyncio'
 
 
-def test_v4_classify_devices_without_apikey():
-    response = client.post("/v4/classify_devices")
+@pytest.fixture
+async def client():
+    async with LifespanManager(app) as manager:
+        async with AsyncClient(transport=ASGITransport(app=manager.app), base_url="http://test") as client:
+            yield client
+
+
+@pytest.mark.anyio
+async def test_v4_classify_devices_without_apikey(client: AsyncClient):
+    response = await client.post("/v4/classify_devices")
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-def test_v4_classify_devices_without_body():
+@pytest.mark.anyio
+async def test_v4_classify_devices_without_body(client: AsyncClient):
     """422 so that it is equal with v3"""
-    response = client.post("/v4/classify_devices", headers={"X-API-Key": settings.api_key})
+    response = await client.post("/v4/classify_devices", headers={"X-API-Key": settings.api_key})
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
