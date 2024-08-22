598d00134e9707e1faf018f17a1689513f81eba5
lhoeke <l.hoeke@gmx.de>
2024-08-22 19:18:30 +0200

    delete blank lines

--- ml_api/src/classification/classify_devices.py
+++ ml_api/src/classification/classify_devices.py
@@ -22,17 +22,13 @@ device_mapping_inverse = {v: k for k, v in device_mapping.items()}
 def preprocess_electricity_data(electricity_data):
     # Extract the power values from ElectricityInput
     power_values = [entry.power for entry in electricity_data]
-    
     # Convert the list into a numpy array that model can handle inputs
     time_series_array = np.expand_dims(np.array(power_values), axis=-1)
-    
     return time_series_array
 
 def predict(electricity_consumption: DeviceClassificationRequest) -> DeviceClassificationResponse:
-
     results = []
     model = models["device_classification"]
-
     for peaks in electricity_consumption.peaks:
         time_series_padded = preprocess_electricity_data(peaks.electricity)
         predicted_probabilities = model.predict(np.array([time_series_padded]))
@@ -41,5 +37,4 @@ def predict(electricity_consumption: DeviceClassificationRequest) -> DeviceClass
             device_name = device_mapping_inverse.get(i, f"Unknown Device {i}")
             classified_devices.append(ClassifiedDevices(name=device_name, confidence=float(prob)))
         results.append(PeakOutput(peak_id=peaks.peak_id, devices=classified_devices))
-
     return DeviceClassificationResponse(peaks=results)
